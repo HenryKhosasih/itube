5ad585172f4ca9b50e999955ad608bfb872c7a1f
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-04-15 23:22:03 +1000

    Implement Search Feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Grid } from "@material-ui/core";
 import youtube from './api/youtube';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Grid item xs={11}>
         <Grid container spacing={10}>
           <Grid item xs={12}>
-            {/* {SearchBar} */}
+            <SearchBar onSubmit= {handleSubmit} />
           </Grid>
           <Grid item xs={8}>
             {/* {VideoDetail} */}
@@ -20,6 +21,18 @@ function App() {
       </Grid>
     </Grid>
   );
+
+  async function handleSubmit(searchItem) {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyA3aCUHCnmkbVso4f0kInWr5dVDSKpA_1k",
+        q: searchItem,
+      }
+    });
+    console.log(response);
+  }
 }
 
 export default App;
