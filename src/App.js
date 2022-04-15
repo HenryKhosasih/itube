d4c21d9ffaf1aef1f3baa0d9e1a1035e70e8f3e2
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-04-15 22:57:08 +1000

    Set up Youtube API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { Grid } from "@material-ui/core";
+import youtube from './api/youtube';
 
 function App() {
   return (
-    <div className="App">
-      <h1>iTube</h1>
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            {/* {SearchBar} */}
+          </Grid>
+          <Grid item xs={8}>
+            {/* {VideoDetail} */}
+          </Grid>
+          <Grid item xs={4}>
+            {/* {VideoList} */}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
